1b66d939ad700652be146e1112f715117b1c6ded
grantnilson <gnilson314@gmail.com>
2023-03-11 18:39:35 -0800

    user auth

--- .history/pages/_app_20230311175917.tsx
+++ .history/pages/_app_20230311175917.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
 
 function MyApp({
   Component,
--- .history/pages/_app_20230311182933.tsx
+++ .history/pages/_app_20230311182933.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../styles/global.css";
 
 function MyApp({
   Component,
