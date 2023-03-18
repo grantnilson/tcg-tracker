0b4e4b7c28f12f0c2043a0974d53e14bdfc90450
grantnilson <gnilson314@gmail.com>
2023-03-17 21:04:50 -0700

    added decks database types and schema on supabase - history

--- .history/components/Deck_20230317203650.tsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-import {
-  useUser,
-  useSupabaseClient,
-  Session,
-} from "@supabase/auth-helpers-react";
-import { Database } from "../utils/database.types";
-
-type Profiles = Database["public"]["Tables"]["decks"]["Row"];
--- .history/pages/Accounts/index_20230311200618.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-import Account from "../../components/Account";
-
-type Props = {};
-
-export default function Accounts() {}
