6998a579dc4522761d326c989992467e6135d0ff
Grant Nilson <gnilson314@gmail.com>
2023-09-26 11:54:10 -0700

    changed navbar colors, added menu item for navbar with router links

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -15,6 +15,10 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Link from "next/link";
+
+const pages = ["Accounts", "Decks"];
 
 export default function Navbar() {
   const [anchorNav, setAnchorNav] = React.useState<null | HTMLElement>(null);
@@ -35,9 +39,125 @@ export default function Navbar() {
   const handleCloseUserMenu = () => {
     setAnchorUserMenu(null);
   };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#474747",
+        // light: will be calculated from palette.primary.main,
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contrast with palette.primary.main
+      },
+      secondary: {
+        main: "#474747",
+        light: "#F5EBFF",
+        // dark: will be calculated from palette.secondary.main,
+        contrastText: "#fff",
+      },
+    },
+  });
+
   return (
-    <AppBar position="static" color="primary" enableColorOnDark>
-      <div>Navbar</div>
-    </AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" color="primary" enableColorOnDark>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                TCG-Tracker
+              </Typography>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                color="primary"
+                anchorEl={anchorNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem
+                    color="primary"
+                    key={page}
+                    //onClick={() => router.push(`/${page}`)}
+                  >
+                    <Link href={`/${page}`} passHref>
+                      <Typography color="primary" textAlign="center">
+                        {page}
+                      </Typography>
+                    </Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              TCG-Tracker
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 }
