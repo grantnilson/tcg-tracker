fae5867bc07f2ee139110f042ea4bbce65926cbe
Grant Nilson <gnilson314@gmail.com>
2023-10-02 19:12:14 -0700

    changed user id to just fetch from session

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -20,18 +20,75 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Link from "next/link";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { Database } from "@/utils/database.types";
+//import { Session, User } from "@supabase/gotrue-js/src/lib/types";
 
 import AvatarImage from "../AvatarImage";
 
 type Profiles = Database["public"]["Tables"]["profiles"]["Row"];
+
+interface Session {
+  /**
+   * The oauth provider token. If present, this can be used to make external API requests to the oauth provider used.
+   */
+  provider_token?: string | null;
+  /**
+   * The oauth provider refresh token. If present, this can be used to refresh the provider_token via the oauth provider's API.
+   * Not all oauth providers return a provider refresh token. If the provider_refresh_token is missing, please refer to the oauth provider's documentation for information on how to obtain the provider refresh token.
+   */
+  provider_refresh_token?: string | null;
+  /**
+   * The access token jwt. It is recommended to set the JWT_EXPIRY to a shorter expiry value.
+   */
+  access_token: string;
+  /**
+   * A one-time used refresh token that never expires.
+   */
+  refresh_token: string;
+  /**
+   * The number of seconds until the token expires (since it was issued). Returned when a login is confirmed.
+   */
+  expires_in: number;
+  /**
+   * A timestamp of when the token will expire. Returned when a login is confirmed.
+   */
+  expires_at?: number;
+  token_type: string;
+  user: User;
+}
+
+interface User {
+  id: string;
+  app_metadata: {};
+  user_metadata: {};
+  aud: string;
+  confirmation_sent_at?: string;
+  recovery_sent_at?: string;
+  email_change_sent_at?: string;
+  new_email?: string;
+  new_phone?: string;
+  invited_at?: string;
+  action_link?: string;
+  email?: string;
+  phone?: string;
+  created_at: string;
+  confirmed_at?: string;
+  email_confirmed_at?: string;
+  phone_confirmed_at?: string;
+  last_sign_in_at?: string;
+  role?: string;
+  updated_at?: string;
+  identities?: [];
+  factors?: [];
+}
 
 const pages = ["Decks"];
 const userSettings = ["Accounts"];
 
 export default function Navbar() {
   const supabase = useSupabaseClient<Database>();
+  const [session, setSession] = useState<any>();
 
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<any>();
   const [loading, setLoading] = useState(false);
   const [userAvatar, setUserAvatar] = useState<any>(null);
 
@@ -41,17 +98,20 @@ export default function Navbar() {
         setLoading(true);
 
         // Fetch user
-        const { data: userData } = await supabase.auth.getUser();
+        //const { data: userData } = await supabase.session.getUser();
         const { data: sessionData, error } = await supabase.auth.getSession();
         console.log("session data", sessionData);
-        if (userData && userData.user) {
-          setUser(userData.user);
+
+        const user_id = sessionData.session?.user?.id;
+
+        if (sessionData) {
+          setSession(sessionData.session);
         } else {
-          setUser(null);
+          setSession(null);
         }
 
-        // Fetch user avatar
-        if (userData && userData.user) {
+        // Fetch user avatars
+        if (user_id && session) {
           const {
             data: avatarData,
             error,
@@ -59,7 +119,7 @@ export default function Navbar() {
           } = await supabase
             .from("profiles")
             .select(`avatar_url`)
-            .eq("id", userData.user.id)
+            .eq("id", user_id)
             .single();
 
           if (error && status !== 406) {
