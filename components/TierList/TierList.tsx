ec00331f4cffe51b6e39557b6665559cce573e50
Grant Nilson <gnilson314@gmail.com>
2023-10-26 16:51:55 -0700

    incremental group sorting

--- components/TierList/TierList.tsx
+++ components/TierList/TierList.tsx
@@ -17,19 +17,44 @@ export const TierListPage = () => {
       const { data: decks, error } = await supabase
         .from("decks")
         .select("*")
+        .order("tier")
         .order("time_added", { ascending: true });
 
       if (error) console.log("error : ", error);
       else {
         setDecks(decks);
-        setState([transformData(decks)]);
+        //setState([transformData(decks)]);
+        const groupedDecks = groupAndSortDecks(decks);
+        console.log(groupedDecks);
+        setState(groupedDecks);
       }
     };
 
     fetchDecks();
   }, [supabase]);
 
-  //console.log("decks : ", decks);
+  function groupAndSortDecks(decks: Decks[]): any[] {
+    const grouped: any = {};
+
+    decks.forEach((deck) => {
+      const tier = deck.tier || "Unsorted"; // Default to "Unsorted" if no tier
+      if (!grouped[tier]) {
+        grouped[tier] = [];
+      }
+      grouped[tier].push(transformData([deck])[0]); // Apply the same transformation
+    });
+
+    // Sort decks within each group by "elo"
+    for (const tier in grouped) {
+      grouped[tier].sort((a: any, b: any) => b.elo - a.elo);
+    }
+
+    // Convert grouped decks to an array
+    return Object.keys(grouped).map((tier) => ({
+      tier,
+      decks: grouped[tier],
+    }));
+  }
 
   function transformData(inputData: Decks[]) {
     return inputData.map((item) => ({
@@ -53,10 +78,6 @@ export const TierListPage = () => {
     droppableSource: { droppableId: number; index: number },
     droppableDestination: { droppableId: number; index: number }
   ) => {
-    console.log("move source : ", source);
-    console.log("move destination : ", destination);
-    console.log("move droppable source : ", droppableSource);
-    console.log("move droppable destination", droppableDestination);
     const sourceClone = Array.from(source);
     const destClone = Array.from(destination);
     const [removed] = sourceClone.splice(droppableSource.index, 1);
@@ -73,15 +94,10 @@ export const TierListPage = () => {
   const grid = 8;
 
   const getItemStyle = (isDragging: any, draggableStyle: any) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: "none",
     padding: grid * 2,
     margin: `0 0 ${grid}px 0`,
-
-    // change background colour if dragging
     background: isDragging ? "lightgreen" : "grey",
-
-    // styles we need to apply on draggables
     ...draggableStyle,
   });
 
@@ -91,15 +107,8 @@ export const TierListPage = () => {
     width: 250,
   });
 
-  //const [state, setState] = useState([transformData(decks)]);
-  //console.log("state:", state);
-
   function onDragEnd(result: any) {
     const { source, destination } = result;
-    console.log("source on drag end: ", source);
-    console.log("destination on drag end: ", destination);
-
-    // dropped outside the list
     if (!destination) {
       return;
     }
@@ -127,66 +136,75 @@ export const TierListPage = () => {
       <button
         type="button"
         onClick={() => {
-          setState([...state, []]);
+          setState([...state, { tier: "New Group", decks: [] }]);
         }}
       >
         Add new group
       </button>
       <div style={{ display: "flex" }}>
         <DragDropContext onDragEnd={onDragEnd}>
-          {state.map((el: any, ind: any) => (
-            <Droppable key={ind} droppableId={`${ind}`}>
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  style={getListStyle(snapshot.isDraggingOver)}
-                  {...provided.droppableProps}
-                >
-                  {el.map((item: any, index: any) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.id}
-                      index={index}
+          {/* {console.log("state : ", state)} */}
+          {state.map((el: any, ind: any) => {
+            // console.log("el: ", el);
+            return (
+              <Droppable key={ind} droppableId={`${ind}`}>
+                {(provided, snapshot) => {
+                  // console.log("provided: ", provided);
+                  // console.log("snapshot", snapshot);
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      style={getListStyle(snapshot.isDraggingOver)}
+                      {...provided.droppableProps}
                     >
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={getItemStyle(
-                            snapshot.isDragging,
-                            provided.draggableProps.style
-                          )}
+                      <h2>{el.tier}</h2>
+                      {el.decks.map((item: any, index: any) => (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
                         >
-                          <div
-                            style={{
-                              display: "flex",
-                              justifyContent: "space-around",
-                            }}
-                          >
-                            {item.name}
-                            <button
-                              type="button"
-                              onClick={() => {
-                                const newState = [...state];
-                                newState[ind].splice(index, 1);
-                                setState(
-                                  newState.filter((group) => group.length)
-                                );
-                              }}
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              style={getItemStyle(
+                                snapshot.isDragging,
+                                provided.draggableProps.style
+                              )}
                             >
-                              delete
-                            </button>
-                          </div>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          ))}
+                              <div
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "space-around",
+                                }}
+                              >
+                                {item.name}
+                                {/* <button
+                                  type="button"
+                                  onClick={() => {
+                                    const newState = [...state];
+                                    newState[ind].splice(index, 1);
+                                    setState(
+                                      newState.filter((group) => group.length)
+                                    );
+                                  }}
+                                >
+                                  delete
+                                </button> */}
+                              </div>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            );
+          })}
         </DragDropContext>
       </div>
     </div>
