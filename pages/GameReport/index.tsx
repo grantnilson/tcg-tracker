c370c6de5fa17908763d7037bfec57b5de53a18a
Grant Nilson <gnilson314@gmail.com>
2023-11-24 17:48:02 -0800

    added basic select

--- pages/GameReport/index.tsx
+++ pages/GameReport/index.tsx
@@ -1,5 +1,8 @@
+import { GameReportPage } from "@/components/GameReport/GameReport";
 export default function GameReport() {
   return (
-    <div className="container" style={{ padding: "50px 0 100px 0" }}></div>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      <GameReportPage />
+    </div>
   );
 }
